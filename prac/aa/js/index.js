17d1c9df28e11bce85cb88769e196d0814544934
h16ro <86038385+h16ro@users.noreply.github.com>
2021-06-23 10:09:02 +0900

    Add files via upload

---
+++ prac/aa/js/index.js
@@ -0,0 +1,17 @@
+'use strict'
+const birthday = {
+    year: 2001,
+    month: 1,
+    date: 26
+  };
+function getAge(birthday){
+    let today = new Date();
+    let thisYearsBirthday = new Date(today.getFullYear(), birthday.month-1, birthday.date);
+    let age = today.getFullYear() - birthday.year;
+    if(today < thisYearsBirthday){
+        age--;
+    }
+    return age;
+}
+document.write(getAge(birthday)+"歳");
+
